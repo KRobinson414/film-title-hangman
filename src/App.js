e918deb6dd2c4099d7106ad6f6b4766979ee5cfe
Katie Robinson <katierobinson110589@gmail.com>
2019-02-13 15:05:08 +0000

    created blank components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import Category from "./components/Category";
+import Image from "./components/Image";
+import Word from "./components/Word";
+import Letters from "./components/Letters";
+import Button from "./components/Button";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Category />
+        <Image />
+        <Word />
+        <Letters />
+        <Button />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Letters.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Letters extends Component {
+  render() {
+    return <div />;
+  }
+}
